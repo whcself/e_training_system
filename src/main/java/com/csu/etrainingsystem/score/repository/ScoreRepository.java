e54bbd56b88495212c16884ea11eb40687974e51
scjn <642407967@qq.com>
2018-12-10 23:14:39 +0800

    计算总成绩，查询成绩修改记录

--- src/main/java/com/csu/etrainingsystem/score/repository/ScoreRepository.java
+++ src/main/java/com/csu/etrainingsystem/score/repository/ScoreRepository.java
@@ -19,23 +19,7 @@ import java.util.Optional;
 @Repository
 public interface ScoreRepository extends JpaRepository<Score, Integer> {
 
-    // TODO: 2018/11/29 scjn 计算总成绩
     @Modifying
-//    @Query(value = "UPDATE student" +
-//            "INNER JOIN (" +
-//            "SELECT" +
-//            "newscore.sid," +
-//            "sum( newscore.pro_score * p.weight ) AS total_score " +
-//            "FROM" +
-//            "( SELECT s.*, stu.batch_name FROM score AS s, student AS stu WHERE s.sid = stu.sid ) AS newscore," +
-//            "proced AS p " +
-//            "WHERE" +
-//            "newscore.batch_name = p.batch_name " +
-//            "AND newscore.pro_name = p.pro_name and newscore.batch_name=?1" +
-//            "GROUP BY" +
-//            "newscore.sid " +
-//            ") total ON student.sid = total.sid " +
-//            "SET student.total_score = total.total_score",nativeQuery = true)
     @Query(value = "UPDATE student INNER JOIN (SELECT newscore.sid, sum( newscore.pro_score * p.weight ) AS total_score  FROM " +
             "( SELECT s.*, stu.batch_name FROM score AS s, student AS stu WHERE s.sid = stu.sid ) AS newscore, proced AS p  " +
             "WHERE newscore.batch_name = p.batch_name AND newscore.pro_name = p.pro_name  and newscore.batch_name=? GROUP BY newscore.sid  ) total " +
