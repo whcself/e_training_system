72894721104214a92675b595a13d4fc63aa1fe0c
whcself <32793449+whcself@users.noreply.github.com>
2018-12-22 20:56:35 +0800

    跨域新问题11

--- src/main/java/com/csu/etrainingsystem/ETrainingSystemApplication.java
+++ src/main/java/com/csu/etrainingsystem/ETrainingSystemApplication.java
@@ -4,14 +4,20 @@ import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 //(exclude = {DataSourceAutoConfiguration.class})
-@SpringBootApplication // exclude: because it does not RedisConfig the dataSource yet.
+@SpringBootApplication
+@EnableScheduling
+@EnableCaching
+@EnableAsync
 @EnableSwagger2
 public class ETrainingSystemApplication {
     public static void main(String[] args) {
