eb4ac0f6f8c8ed2d485455d4d50d9f7f58c71f26
scjn <642407967@qq.com>
2019-02-22 19:49:02 +0800

    student add check, whether there is spstudent

--- src/main/java/com/csu/etrainingsystem/student/service/StudentService.java
+++ src/main/java/com/csu/etrainingsystem/student/service/StudentService.java
@@ -65,8 +65,8 @@ public class StudentService {
         if(studentRepository.findStudentBySid(id).isPresent()){
             throw new Exception("#学号重复，该学生已经存在"+id+"#");
         }
-        if(userRepository.findUserByAccount(id)==null){
-            throw new Exception("#账号重复，该用户已存在"+id+"#");
+        if(spStudentRepository.findSpStudentBySid(id)==null){
+            throw new Exception("#账号重复，该学生已存在，为特殊学生"+id+"#");
         }
         User user=new User ();
         user.setRole ("student");
